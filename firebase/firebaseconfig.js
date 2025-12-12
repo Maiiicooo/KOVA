2017e941053c85dd36fe4f005c96b94250ed07de
Maiiicooo <166993007+Maiiicooo@users.noreply.github.com>
2025-12-12 14:36:14 +0100

    Add files via upload

---
+++ firebase/firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyARxFI9HyJxrINfzDazhxFHKSrGLX8KoBk",
+  authDomain: "kova-6052a.firebaseapp.com",
+  projectId: "kova-6052a",
+  storageBucket: "kova-6052a.firebasestorage.app",
+  messagingSenderId: "873689812978",
+  appId: "1:873689812978:web:e0581f3df823e51239656b",
+  measurementId: "G-VM5224Z3K6",
+};
